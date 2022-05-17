ae0cf4a29ba318cf00e5579ab4cdbc7dcf6555f1
marsianca <xmarsiancax@mail.ru>
2022-05-17 16:05:03 +0300

    добавила оптимизацию при загрузки картинки сначала,картинка подгружается размытая

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.scss'
 import Layout from "../components/Layout";
+import mobile from "../public/mobile-phone.jpg"
+import Image from "next/Image";
 
-function MyApp({ Component, pageProps }) {
-  return  <Layout>
-     <Component {...pageProps} />
-  </Layout>
+function MyApp({Component, pageProps}) {
+    return <Layout>
+        <main>
+            <Component {...pageProps} />
+        </main>
+        <Image src={mobile}
+               alt={"mobile-phone"}
+               placeholder="blur"
+        />
+    </Layout>
 
 }
 
