20b0db5d9a7eee5fca4cb56e800750b715ead08e
marsianca <xmarsiancax@mail.ru>
2022-05-17 15:29:10 +0300

    добавила стиль для активной ссылки добавила redirect на главную со стр 404.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,24 @@
 import s from "../styles/Navbar.module.scss"
 import Link from "next/link";
+import {useRouter} from "next/router";
 
+const navigation = [
+    {id: 1, title: "Home", path: "/"},
+    {id: 2, title: "Posts", path: "/posts"},
+    {id: 3, title: "Contacts", path: "/contacts"},
+]
 const Navbar = () => {
+    const {pathname} = useRouter();
     return <div className={s.nav}>
-       <nav className={s.nav}>
-          <div className={s.logo}>
-              logo
-          </div>
-           <div className={s.links}>
-               <Link href={"/"}><a>Home</a></Link>
-               <Link href={"/posts"}><a>Posts</a></Link>
-               <Link href={"/contacts"}><a>Contacts</a></Link>
-           </div>
-       </nav>
+        <nav className={s.nav}>
+            <div className={s.logo}>
+                logo
+            </div>
+            <div className={s.links}>
+                {navigation.map(({id, title, path}) =>
+                    (<Link key={id} href={path}><a className={pathname === path ? s.active : null}>{title}</a></Link>))}
+            </div>
+        </nav>
     </div>
 }
 export default Navbar
--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,14 @@
 import Heading from "../components/Heading";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const Error = () => {
+    const router = useRouter()
+    useEffect(()=>{
+        setTimeout(()=>{router.push('/')},
+            3000)
+    }, [router])
+
     return <>
         <Heading text="404"/>
         <Heading tag="h2" text="Something is going wrong..."/>
