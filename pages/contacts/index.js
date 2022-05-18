76d95983e108fe87012a486d31e500402f39cc2b
marsianca <xmarsiancax@mail.ru>
2022-05-18 11:05:17 +0300

    добавила метод getStaticProps для получения contacts

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,33 @@
 import Heading from "../../components/Heading";
 import Head from 'next/head'
 
+export const getStaticProps = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const data = await response.json()
 
-const Contacts = () => {
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+    return {
+        props: {contacts: data}
+    }
+}
+
+const Contacts = ({contacts}) => {
+
     return <>
         <Head>
             <title>Contacts</title>
         </Head>
-        <Heading text="Contacts list"/>
+        <Heading text="Contacts list:"/>
+        <ul>
+            {contacts && contacts.map(({id, name, email}) => (
+                    <li key={id}><strong>{name}</strong><span> {email}</span></li>
+                )
+            )}
+        </ul>
     </>
 }
 export default Contacts
