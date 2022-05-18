c3a0fdebcb7d25031b9a34005e62b44b6616da8f
marsianca <xmarsiancax@mail.ru>
2022-05-18 10:08:46 +0300

    изменила шрифт глобально

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import '../styles/globals.scss'
 import Layout from "../components/Layout";
 import mobile from "../public/mobile-phone.jpg"
 import Image from "next/Image";
+import Head from 'next/head'
 
 function MyApp({Component, pageProps}) {
     return <Layout>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Lato:ital,wght@1,300&family=Montserrat:ital,wght@1,300&display=swap"
+                  rel="stylesheet"/>
+        </Head>
         <main>
             <Component {...pageProps} />
         </main>
