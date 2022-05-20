9201215ef7d26fb5d0d58a80eac43fd8aeed8daf
marsianca <xmarsiancax@mail.ru>
2022-05-20 10:41:24 +0300

    vercel deploy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import s from "../styles/Navbar.module.scss"
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import {useRouter} from "next/router";
 import logo from "../public/logo.png"
 import {FC} from "react"
